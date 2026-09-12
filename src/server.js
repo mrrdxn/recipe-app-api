a0ff77164083de1e14721d2d2b111f63743933f9
mrrdxn <martindixon2g1@gmail.com>
2026-09-11 20:53:34 -0500

    fix schema import path

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import { ENV } from "./config/env.js";
 import {db} from "./config/db.js";
-import { favoritesTable } from "./db/dbSchema.js";
+import { favoritesTable } from "./db/schema.js";
 import { eq, and } from "drizzle-orm";
 
 const app = express();
